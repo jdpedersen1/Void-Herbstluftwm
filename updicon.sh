8e0ee1c253016e5b273ee21d7a4abd0e545b291b
Jacob <jdpedersen1@yahoo.com>
2022-11-14 07:25:24 -0600

    updated some themes and wallpaper dir issuess

--- updicon.sh
+++ updicon.sh
@@ -2,35 +2,48 @@
 
 # Created By: Jake@Linux
 # Created On: Thu 25 Aug 2022 02:20:52 PM CDT
-# Project: update icon
+# Project: sys tray icon for system updates
 
-upd="$(sudo xbps-install -nuM | wc -l)"
-note="$(pidof yad)"
+upd="$(sudo xbps-install -SunM | wc -l)"
+list="$(sudo xbps-install -nuM | awk {'print $1'})"
+inst="$(pidof yad)"
 
-if [[ "$upd" < 1 ]];
+
+while true; do
+if [[ $inst -eq " " ]]
 then
-    if [[ "$note"  < 1 ]]; 
-    then
-        yad --notification --image=$HOME/Desktop/icon/noupdate.png --command=notify-send "$(sudo xbps-install -nuM | awk {'print $1'})"
-    else
-        pkill yad;
-        yad --notification --image=$HOME/Desktop/icon/noupdate.png --command=notify-send "$(sudo xbps-install -nuM | awk {'print $1'})"
-    fi
-elif [[ "$upd" -le 5 ]];
+if (( $upd == 0 ));
 then
-    if [[ "$note" < 1 ]]; 
-    then
-        yad --notification --image=$HOME/Desktop/icon/modupdate.png --command=notify-send "$(sudo xbps-install -nuM | awk {'print $1'})"
-    else
-        pkill yad;
-        yad --notification --image=$HOME/Desktop/icon/modupdate.png --command=notify-send "$(sudo xbps-install -nuM | awk {'print $1'})"
-    fi
-else
-    if [[ "$note" < 1 ]];
-    then
-        yad --notification --image=$HOME/Desktop/icon/lotsupdate.png --command=notify-send "$(sudo xbps-install -nuM | awk {'print $1'})"
-    else
-        pkill yad;
-        yad --notification --image=$HOME/Desktop/icon/lotsupdate.png --command=notify-send "$(sudo xbps-install -nuM | awk {'print $1'})"
-    fi
+    cat $HOME/.config/herbstluftwm/updateIcons/noupdate.png > $HOME/.config/herbstluftwm/updateIcons/trayIcon.png
+    yad --text "$upd Updates" --notification --image=$HOME/.config/herbstluftwm/updateIcons/trayIcon.png
+elif (( $upd <= 5 ));
+then
+    cat $HOME/.config/herbstluftwm/updateIcons/modupdate.png > $HOME/.config/herbstluftwm/updateIcons/trayIcon.png
+    yad --text "$upd Updates: Right click to see all updates, Left click to perform update" --notification\
+        --image=$HOME/.config/herbstluftwm/updateIcons/trayIcon.png --menu="$list" --command "alacritty -e sudo xbps-install -Su"
+elif (( $upd > 5 ));
+then
+    cat $HOME/.config/herbstluftwm/updateIcons/lotsupdate.png > $HOME/.config/herbstluftwm/updateIcons/trayIcon.png
+    yad --text "$upd Updates: Right click to see all updates, Left click to perform update" --notification\
+        --image=$HOME/.config/herbstluftwm/updateIcons/trayIcon.png --menu="$list" --command "alacritty -e sudo xbps-install -Su"
 fi
+elif [[ $inst -gt "1" ]]
+then
+if (( $upd == 0 ));
+then
+    cat $HOME/.config/herbstluftwm/updateIcons/noupdate.png > $HOME/.config/herbstluftwm/updateIcons/trayIcon.png
+    yad --text "$upd Updates" --notification --image=$HOME/.config/herbstluftwm/updateIcons/trayIcon.png
+elif (( $upd <= 5 ));
+then
+    cat $HOME/.config/herbstluftwm/updateIcons/modupdate.png > $HOME/.config/herbstluftwm/updateIcons/trayIcon.png
+    yad --text "$upd Updates: Right click to see all updates, Left click to perform update" --notification\
+        --image=$HOME/.config/herbstluftwm/updateIcons/trayIcon.png --menu="$list" --command "alacritty -e sudo xbps-install -Su"
+elif (( $upd > 5 ));
+then
+    cat $HOME/.config/herbstluftwm/updateIcons/lotsupdate.png > $HOME/.config/herbstluftwm/updateIcons/trayIcon.png
+    yad --text "$upd Updates: Right click to see all updates, Left click to perform update" --notification\
+        --image=$HOME/.config/herbstluftwm/updateIcons/trayIcon.png --menu="$list" --command "alacritty -e sudo xbps-install -Su"
+fi
+fi
+sleep 20
+done
